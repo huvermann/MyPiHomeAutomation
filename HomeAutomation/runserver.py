783755cca3250781c2f3d318628555af55c633cf
Heiko Huvermann <wp7dev@huvermann.com>
2015-07-31 18:44:26 +0200

    Changed host ip and removed old project

--- HomeAutomation/runserver.py
+++ HomeAutomation/runserver.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 This script runs the HomeAutomation application using a development server.
 """
 
@@ -6,7 +6,7 @@ from os import environ
 from HomeAutomation import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
