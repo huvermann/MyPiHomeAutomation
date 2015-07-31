896d0a26a4ac190578e7b438b7144d211deb992d
Heiko Huvermann <wp7dev@huvermann.com>
2015-07-31 16:55:27 +0200

    Adding project

---
+++ HomeAutomation/HomeAutomation/views.py
@@ -0,0 +1,29 @@
+﻿"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template, Response, jsonify
+from HomeAutomation import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'switches.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/switches')
+def switches():
+    return render_template(
+        'switches.html'
+    )
+
+@app.route('/gpio', methods=["GET", "POST"])
+def jsongpio():
+    # GPIO-Manager einbauen!!
+    dat = [{"name":"gpio-0","value":"off"},{"name":"gpio-1","value":"off"},{"name":"gpio-2","value":"on"},{"name":"gpio-3","value":"off"},{"name":"gpio-4","value":"on"}]
+    return jsonify(gpio=dat)
---
+++ HomeAutomation/runserver.py
@@ -0,0 +1,14 @@
+"""
+This script runs the HomeAutomation application using a development server.
+"""
+
+from os import environ
+from HomeAutomation import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
