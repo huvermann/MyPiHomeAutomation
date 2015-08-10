bb8a31b215a148a649cde0be513ebc027c49b14d
Heiko Huvermann <wp7dev@huvermann.com>
2015-08-10 08:23:34 +0200

    Implementet IOPort blink prototype

--- HardwareServiceClient/blink.py
+++ HardwareServiceClient/blink.py
@@ -1,5 +1,8 @@
-import time
+﻿import time
 import RPi.GPIO as GPIO
+import ptvsd
+ptvsd.enable_attach('iot')
+ptvsd.wait_for_attach()
 
 # RPi.GPIO Layout verwenden (wie Pin-Nummern)
 GPIO.setmode(GPIO.BOARD)
@@ -17,3 +20,4 @@ while 1:
   GPIO.output(16, GPIO.LOW)
   time.sleep(1)
   GPIO.output(16, GPIO.HIGH)
+  time.sleep(1)
--- HardwareServiceClient/driveport.py
+++ HardwareServiceClient/driveport.py
@@ -1,6 +1,11 @@
-import os
+﻿import os
 import sys, getopt
 import platform
+import ptvsd
+#ptvsd.enable_attach('iot')
+#ptvsd.wait_for_attach()
+
+
 
 def printHelp():
     print
@@ -14,19 +19,52 @@ def printHelp():
     print 'python driveport.py -h'
     print
 
-def executeOnLinux(port, direction, value):
-    print "Not implemented"
+def readPort(port):
+    print "readPort"
     pass
+
+def writePort(port, value):
+    print "writePort"
+    import RPi.GPIO as GPIO
+
+    # RPi.GPIO Layout verwenden (wie Pin-Nummern)
+    try:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        # Pin 16 (GPIO 23) auf Output setzen
+        GPIO.setup(16, GPIO.OUT)
+        # Wert setzen:
+        if value == "1":
+            GPIO.output(16, GPIO.HIGH)
+            print 1
+        else:
+            GPIO.output(16, GPIO.LOW)
+            print 0
+    finally:
+        #GPIO.cleanup()
+        pass
+
+def executeOnLinux(port, direction, value):
+    if direction:
+        if direction == 'in':
+            readPort(port)
+        elif direction == 'out':
+            writePort(port, value)
+        else:
+            print "Error: Direction can be -d out or -in"
+
 def executeOnWindows(port, direction, value):
     print "port :%s" % (port)
     print "direction: %s" % (direction)
     print "value: %s" % (value)
+    cmd = '"C:\Program Files (x86)\PuTTY\plink.exe" pi@192.168.2.108 -pw hhk13mi sudo python ~/webserver/driveport.py -p %s -d %s -v %s' % (port, direction, value)
+    os.system(cmd)
     pass
 
 def executeArguments(port, direction, value):
-    if platform.system == 'Linux':
+    if platform.system() == 'Linux':
         executeOnLinux(port, direction, value)
-    elif platform.system == "Windows":
+    elif platform.system() == "Windows":
         executeOnWindows(port, direction, value)
         
 
@@ -36,7 +74,7 @@ def main(argv):
     except getopt.GetoptError:
         printHelp()
         sys.exit(2)
-    
+
     port = ''
     direction = ''
     value = ''    
